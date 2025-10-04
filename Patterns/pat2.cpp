056d493233173a38490427b664b53e4c6b284653
somya <somyasharma0488@gmail.com>
2025-10-04 09:23:23 +0530

    main

---
+++ Patterns/pat2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+
+    int i=1;
+    // while(i<=n){
+    //     int j=1;
+    //     while(j<=n){
+    //         char ch='A'+i-1;
+    //         cout<<ch<<" ";
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+
+    // while(i<=n){
+    //     int j=1;
+    //     while(j<=n){
+    //         char ch='A'+j-1;
+    //         cout<<ch<<" ";
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+
+
+
+        while(i<=n){
+        int j=1;
+        while(j<=n){
+            char ch='A'+i+j-1;
+            cout<<ch<<" ";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
